d65f47c8e6722fb093e23f3dbe64de3004adf700
myra <myraramdenbourg@gmail.com>
2020-07-08 20:28:33 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Myra Ramdenbourg on 7/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
