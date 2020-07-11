9d347740f40465d1260459da148e28eb0371af3b
myra <myraramdenbourg@gmail.com>
2020-07-10 22:18:27 -0500

    done!

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -11,19 +11,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
-    
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.billField.becomeFirstResponder()
+        
         // Do any additional setup after loading the view.
     }
 
- 
-    @IBAction func onTap(_ sender: Any) {
-    view.endEditing(true)
-        
-    }
+    
+    
+    
     @IBAction func calculateTip(_ sender: Any) {
         let bill = Double(billField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.2]
@@ -35,7 +37,29 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "+$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        let defaults = UserDefaults.standard
+        let intValue = defaults.integer(forKey: "default")
+        
+        tipControl.selectedSegmentIndex = intValue
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
     
 }
 
